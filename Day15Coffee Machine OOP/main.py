b3da3ff19fc478985181139ef54b1e6acd7603ed
RogerTimilsina <timilsinaroger@gmail.com>
2025-11-28 15:40:27 +0545

    complete coffee machine

--- Day15Coffee Machine OOP/main.py
+++ Day15Coffee Machine OOP/main.py
@@ -2,6 +2,7 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+menu = Menu()
 menu_item = MenuItem
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
@@ -16,5 +17,9 @@ while is_on:
         money_machine.report()
     elif choice == "off":
         is_on = False
+    elif choice == "latte" or choice == "espresso" or choice == "cappuccino":
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
     else:
         print("Please enter a valid option.\n")

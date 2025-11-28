2300748f3bdd3c90d8b386517442920ca8d17f76
RogerTimilsina <timilsinaroger@gmail.com>
2025-11-28 15:18:48 +0545

    coffee machine changes

--- Day15Coffee Machine OOP/main.py
+++ Day15Coffee Machine OOP/main.py
@@ -6,5 +6,15 @@ menu_item = MenuItem
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 
-coffee_maker.report()
+is_on = True
+while is_on:
 
+    print("--- Coffee Machine ---")
+    choice = input("Which coffee would you like? (espresso/latte/cappuccino): ")  .lower()
+    if choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif choice == "off":
+        is_on = False
+    else:
+        print("Please enter a valid option.\n")
